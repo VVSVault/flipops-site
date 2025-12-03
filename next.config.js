eaa32dc412e03f96e4fe84e2b59d519ff6d44e3c
Tanner Carlson <tannercarlson@vvsvault.com>
2025-12-03 08:41:23 -0500

    Remove experimental CSS options (caused critters error) - back to default

--- next.config.js
+++ next.config.js
@@ -10,16 +10,8 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // Force CSS extraction - Next.js 15 embeds CSS in JS by default
-  experimental: {
-    optimizeCss: true, // Force separate CSS files
-    cssChunking: 'strict', // Ensure CSS is in separate files
-  },
   // Clerk temporarily removed for CSS debugging
   // transpilePackages: ['@clerk/nextjs', '@clerk/clerk-react'],
-  // experimental: {
-  //   optimizePackageImports: ['@clerk/nextjs'],
-  // },
 }
 
 module.exports = nextConfig
